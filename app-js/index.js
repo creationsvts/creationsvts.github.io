d4f6a33033aa6d71d9bbdd6feab7daf02370cdbc
Dev Front <developermater26@gmail.com>
2021-08-21 17:50:56 -0600

    se importo proudccion y se agrego como un hijo de root

--- app-js/index.js
+++ app-js/index.js
@@ -1,9 +1,11 @@
 import Header from "./components/header.js"
 import EcoBottle from "./components/eco_bottle.js"
 import Caracteristicas from "./components/caracteristicas.js"
+import Produccion from "./components/produccion.js"
 const $root = document.getElementById('root')
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(Header())
     $root.appendChild(EcoBottle())
     $root.appendChild(Caracteristicas())
+    $root.appendChild(Produccion())
 })
