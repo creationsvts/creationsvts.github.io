ae3e5bb715bc87499f0677e3f9d0ce49d32f0d5b
Dev Front <developermater26@gmail.com>
2021-08-21 17:29:00 -0600

    se creo todo le cuerpo de caracteristicas

---
+++ app-js/components/caracteristicas.js
@@ -0,0 +1,40 @@
+const Caracteristicas =()=>{
+    const $Fragment = document.createDocumentFragment()
+    const $caracteristicas = document.createElement('section')
+    const $title = document.createElement('h3')
+    $caracteristicas.classList.add('caracteristicas')
+    const caracteristicasList = [
+        {
+            icon:`fa-cog`,
+            text:'Trabajabilidad'
+        },
+        {
+            icon:`fa-bong`,
+            text:'Resistencia termina y quimica'
+        },
+        {
+            icon:`fa-recycle`,
+            text:'100% Biodegradables'
+        }        
+    ]
+    $title.textContent = 'Caracteristicas de PBS'
+    $title.classList.add('caratersiticas__title')
+    $caracteristicas.appendChild($title)
+    caracteristicasList.forEach(el => {
+        const $caracteristica = document.createElement('article')
+
+        const $caracteristicaText = document.createElement('p')
+        $caracteristicaText.textContent = el.text
+        $caracteristicaText.classList.add('caracteristica__text')
+        const $caracteristicaIcon = document.createElement('i')
+        $caracteristicaIcon.classList.add('caracteristica__icon','fas',el.icon)
+
+        $caracteristica.appendChild($caracteristicaIcon)
+        $caracteristica.appendChild($caracteristicaText)
+        $caracteristicas.appendChild($caracteristica)
+    });
+
+    $Fragment.appendChild($caracteristicas)
+    return $Fragment
+}
+export default Caracteristicas
