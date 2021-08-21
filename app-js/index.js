ec67682e70616edcca8175ae9ace4198e497dc83
Dev Front <developermater26@gmail.com>
2021-08-21 16:28:49 -0600

    se importo la funcino EcoBottle

--- app-js/index.js
+++ app-js/index.js
@@ -1,5 +1,7 @@
 import Header from "./components/header.js"
+import EcoBottle from "./components/eco_bottle.js"
 const $root = document.getElementById('root')
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(Header())
+    EcoBottle()
 })
