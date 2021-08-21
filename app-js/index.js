79f560bcc3dcda99fef41ff692b95f38d9079f70
Dev Front <developermater26@gmail.com>
2021-08-21 17:32:55 -0600

    se impoto caracteristicas.js y se agrego como hijo de root

--- app-js/index.js
+++ app-js/index.js
@@ -1,7 +1,9 @@
 import Header from "./components/header.js"
 import EcoBottle from "./components/eco_bottle.js"
+import Caracteristicas from "./components/caracteristicas.js"
 const $root = document.getElementById('root')
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(Header())
     $root.appendChild(EcoBottle())
+    $root.appendChild(Caracteristicas())
 })
