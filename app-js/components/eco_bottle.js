24798f20c98b60662041a5f24ebaf85ee6f27ec0
Dev Front <developermater26@gmail.com>
2021-08-21 16:48:41 -0600

    se creo todo el cuerpo de la seccion de eco bottle y se exporto en un fragmento

--- app-js/components/eco_bottle.js
+++ app-js/components/eco_bottle.js
@@ -1,2 +1,26 @@
-const EcoBottle = ()=>{console.log('hello word')}
+const EcoBottle = ()=>{
+    const $ecoBottle = document.createElement('section')
+    const $ecoBottleImage = document.createElement('img')
+    const $ecoBottleDescription = document.createElement('article')
+    const $Fragment = document.createDocumentFragment()
+    
+    $ecoBottleDescription.classList.add('eco-bottle__description__box')
+    $ecoBottle.classList.add('eco-bottle')
+    $ecoBottleImage.classList.add('eco-bottle__image')
+    
+    $ecoBottleImage.src = 'resources/image-one.jpg'
+    $ecoBottleDescription.innerHTML = `
+    <h1 class="eco-bottle__title">Eco Bottle</h1>
+    <p class="eco-bottle__description">
+        Son botellas 100% biodegradables
+        gracias el polimero Succinato de polibutileno(PBS)
+        un material muy prometedor para la produccion 
+        y comercializacion de productos compostables.
+    </p>
+    `
+    $ecoBottle.appendChild($ecoBottleDescription)
+    $ecoBottle.appendChild($ecoBottleImage)
+    $Fragment.appendChild($ecoBottle)
+    return $Fragment
+}
 export default EcoBottle
