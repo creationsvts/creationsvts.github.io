196caff8e9cc1858ad8e3eee36be8805e9bfaba6
Dev Front <developermater26@gmail.com>
2021-08-21 18:07:13 -0600

    se impoto la funcion producto y se agrego como un hijo de root

--- app-js/index.js
+++ app-js/index.js
@@ -2,10 +2,12 @@ import Header from "./components/header.js"
 import EcoBottle from "./components/eco_bottle.js"
 import Caracteristicas from "./components/caracteristicas.js"
 import Produccion from "./components/produccion.js"
+import Producto from "./components/producto.js"
 const $root = document.getElementById('root')
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(Header())
     $root.appendChild(EcoBottle())
     $root.appendChild(Caracteristicas())
     $root.appendChild(Produccion())
+    $root.appendChild(Producto())
 })
