ed1fccfda48b5734ba86ba49e80bf74196afb50a
Dev Front <developermater26@gmail.com>
2021-08-21 18:14:30 -0600

    se agrego un titlo  y la imagenes se hicieron hijos de un section

--- app-js/components/producto.js
+++ app-js/components/producto.js
@@ -1,18 +1,25 @@
 const Producto = () =>{
     const $producto = document.createElement('section')
     const Dir = 'resources'
+    const $titleProducto = document.createElement('p')
+    const $imagesBox =document.createElement('article')
     $producto.classList.add('producto')
     const images = [
         `${Dir}/bottle-one.png`,
         `${Dir}/bottle-two.png`,
         `${Dir}/bottle-three.png`
     ]
+    $titleProducto.textContent='Producto'
+    $titleProducto.classList.add('producto__title')
+    $producto.appendChild($titleProducto)
+
     images.forEach(el => {
         const $img = document.createElement('img')
         $img.src = el
         $img.alt = 'bottle'
         $img.classList.add('bottle__image')
-        $producto.appendChild($img)
+        $imagesBox.appendChild($img)
+        $producto.appendChild($imagesBox)
     });
     const $Fragment = document.createDocumentFragment()
     $Fragment.appendChild($producto)
