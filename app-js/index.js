e7e821aeca8897803727483d59b3a00b76ee9762
Dev Front <developermater26@gmail.com>
2021-08-21 19:40:39 -0600

    se agrego un listeber al docuemento este reacciona al click del icono de hambuguerguesa y muesta el menu y hace le itercambio de iconos

--- app-js/index.js
+++ app-js/index.js
@@ -13,3 +13,12 @@ document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(Producto())
     $root.appendChild(Footer())
 })
+document.addEventListener('click',(e)=>{
+    if(e.target.matches('.menu-icon')){
+        document.querySelector('.menu').classList.add('active')
+        document.querySelector('.menu-icon').style.display='none'
+    }else if(e.target.matches('.fa-times')){
+        document.querySelector('.menu').classList.remove('active')
+        document.querySelector('.menu-icon').style.display='block'
+    }
+})
