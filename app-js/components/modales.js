2261008abb2b56352fbc3d2e6296f234c75fa6b6
Dev Front <developermater26@gmail.com>
2021-08-21 22:14:19 -0600

    se creo un modal para el formulario

---
+++ app-js/components/modales.js
@@ -0,0 +1,16 @@
+const WindowMolueForm = ()=>{
+    const $modalForm =document.createElement('section')
+    $modalForm.classList.add('modal__form__box')
+    $modalForm.innerHTML = `
+        <article class="modal__form">
+            <p class="modal__form_">
+                Este formulario de contacto solo es de prueba 
+            </p>
+            <button class="modal__btn">OK</button>
+        </article>
+    `
+    const $Fragmento = document.createDocumentFragment()
+    $Fragmento.appendChild($modalForm)
+    return $Fragmento
+}
+export default WindowMolueForm
