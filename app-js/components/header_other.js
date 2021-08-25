8d86da6b43e5f4e9d532df2e7eb2e7ca0ae4b716
Dev Front <developermater26@gmail.com>
2021-08-25 13:03:27 -0600

    se creo un nuevo componente que contiene un header para pestañas direferente a el inicio

---
+++ app-js/components/header_other.js
@@ -0,0 +1,19 @@
+const HeaderOther =()=>{
+    return`
+    <header class="header-other">
+        <section class="header-other__logo">
+            <img src="resources/zorro-white.png" alt="" class="header-other__logo__image">
+            <h2 class="header-other__logo__title">Creations VTS</h2>
+        </section>
+        <nav>
+            <ul class="menu__other">
+                <li class="menu__item__other"><a href="#inicio" class="menu__link__other">Inicio</a></li>
+                <li class="menu__item__other"><a href="#" class="menu__link__other">Docs</a></li>
+                <li class="menu__item__other"><a href="#video" class="menu__link__other">Video</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    `
+}
+export default HeaderOther
